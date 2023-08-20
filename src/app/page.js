6a414f092219fbbff9d7febac4cc5bc60aceae35
daron976 <daronkwiafe@gmail.com>
2023-08-20 19:53:03 +0200

    adjust home page mobile

--- src/app/page.js
+++ src/app/page.js
@@ -3,15 +3,15 @@ import styles from "./home.module.css";
 import "./home.css";
 
 export const metadata = {
-  title: 'Daron Wiafe',
-  description: 'Welcome to my portfolio',
-}
+  title: "Daron Wiafe",
+  description: "Welcome to my portfolio",
+};
 
 export default function Home() {
   return (
     <>
       <main className={`${styles.content} content flex`} data-testid="home">
-        <div className="home-header">
+        <div className={styles.homeHeader}>
           <h1 className={`flicker ${styles.name}`}>Daron Wiafe</h1>
           <p className={`appear ${styles.subtext}`}>Software developer</p>
           <p className={`appearText ${styles.msg}`}>
